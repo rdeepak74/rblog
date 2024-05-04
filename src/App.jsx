4d6c8a734672eccc16bc9138f77d2d42e67c5443
Deepak Rai <deepakmegtech@gmail.com>
2024-05-04 18:54:57 +0530

    Changes of Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth";
+import {login,logout} from './store/authSlice'
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>A blog app with Appwrite</h1>
-    </>
-  )
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading,setLoading]= useState(true);
+  const dispatch= useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((useData)=>{
+      if(useData){
+        dispatch(login({useData}));
+      }else{
+        dispatch(logout());
+      }
+    })
+    .catch((err)=>{
+      console.log(`Fatching a User present : ${err}`);
+    })
+    .finally(()=>setLoading(false))
+  },[]);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+    
+  ) : null
 }
 
 export default App
